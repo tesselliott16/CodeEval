c5aa0a3b066d4eff787223489c3ef2c62a9e4797
Tess L. Elliott <tlelliott16@gmail.com>
2017-07-14 17:01:53 -0400

    Adding new CodeEval projects

---
+++ CodeEval/MultiplyLists/MultiplyElements.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MultiplyElementsCodeEval
+{
+    class MultiplyElements
+    {
+        static void Main(string[] args)
+        {
+            string f = "MultiplyListsFile.txt";
+            var list = new List<string>();
+            var fileStream = new FileStream(f, FileMode.Open, FileAccess.Read);
+            using (var reader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] result = line.Split('|');
+                    string[] right = result[0].Split(' ');
+                    int[] rightInt = Array.ConvertAll(right, x => int.Parse(x));
+                    string[] left = result[1].Split(' ');
+                    int[] leftInt = Array.ConvertAll(left, x => int.Parse(x));
+                    var final = new List<int>();
+                    for (int c=0; c<right.Length; c++)
+                    {
+                        int result2 = Multiply(rightInt[c], leftInt[c]);
+                        final.Add(result2);
+                    }
+                    Console.WriteLine(string.Join(" ", final.Select(x => x.ToString()).ToArray()));
+                }
+                Console.ReadLine();
+            }
+        }
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ CodeEval/RollerCoaster/RollerCoaster.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollerCoaster
+{
+    class RollerCoaster
+    {
+        static void Main(string[] args)
+        {
+            string f = "RollerCoasterText.txt";
+            var fileStream = new FileStream(f, FileMode.Open, FileAccess.Read);
+            using (var reader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    StringBuilder str = new StringBuilder();
+                    bool uppercase = true;
+                    char[] sentence = line.ToCharArray();
+                    foreach (char letter in sentence)
+                    {
+                        if (char.IsLetter(letter))
+                        {
+                            if (uppercase)
+                            {
+                                str.Append(char.ToUpper(letter));
+                            }
+                            else
+                            {
+                                str.Append(char.ToLower(letter));
+                            }
+                            uppercase = !uppercase;
+                        }
+                        else
+                        {
+                            str.Append(letter);
+                        }
+                    }
+                    string finalSentence = str.ToString();
+                    Console.WriteLine(finalSentence);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
