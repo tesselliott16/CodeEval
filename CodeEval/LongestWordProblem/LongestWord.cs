b83178abe87b6f5b7668547436621a1d7f5b5196
Tess L. Elliott <tlelliott16@gmail.com>
2017-07-17 16:32:22 -0400

    Started LongestWordProblem - not finished yet

---
+++ CodeEval/LongestWordProblem/LongestWord.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWordProblem
+{
+    class LongestWord
+    {
+        class MultiplyElements
+        {
+            static void Main(string[] args)
+            {
+                string f = "LongestWordFile.txt";
+                var list = new List<string>();
+                var fileStream = new FileStream(f, FileMode.Open, FileAccess.Read);
+                using (var reader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] result = line.Split(' ');
+                        var longestLength = new List<string>();
+                        for (int i = 0; i < result.Length; i++)
+                        {
+                            longestLength.Add(StringLength(result[i], result[i+1])); 
+                        }
+
+                        Console.WriteLine(longestLength);
+                    }
+                    Console.ReadLine();
+                }
+            }
+
+            static string StringLength(string x, string y)
+            {
+                return x.Length > y.Length ? x : y;
+            }
+        }
+    }
+}
