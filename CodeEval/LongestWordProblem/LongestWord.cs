5c5746a455479b7724f8ac4b8a6ebcb3f3c1686b
Tess L. Elliott <tlelliott16@gmail.com>
2017-07-18 09:17:53 -0400

    fixed the double classes in LongestWord

--- CodeEval/LongestWordProblem/LongestWord.cs
+++ CodeEval/LongestWordProblem/LongestWord.cs
@@ -9,53 +9,51 @@ namespace LongestWordProblem
 {
     class LongestWord
     {
-        class MultiplyElements
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            string f = @"C:\_Projects\CodeEval\CodeEval\LongestWordProblem\LongestWordFile.txt";
+            var list = new List<string>();
+            var fileStream = new FileStream(f, FileMode.Open, FileAccess.Read);
+            using (var reader = new StreamReader(fileStream, Encoding.UTF8))
             {
-                string f = @"C:\_Projects\CodeEval\CodeEval\LongestWordProblem\LongestWordFile.txt";
-                var list = new List<string>();
-                var fileStream = new FileStream(f, FileMode.Open, FileAccess.Read);
-                using (var reader = new StreamReader(fileStream, Encoding.UTF8))
+                string line;
+                while ((line = reader.ReadLine()) != null)
                 {
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
+                    string longestLength = string.Empty;
+                    string[] result = line.Split(' ');
+                    for (int i = 0; i < result.Length; i++)
                     {
-                        string longestLength = string.Empty;
-                        string[] result = line.Split(' ');
-                        for (int i = 0; i < result.Length; i++)
+                        if (i == 0)
                         {
-                            if (i == 0)
-                            {
-                                longestLength = result[i];
-                            }
-                            if (i > 0)
-                            {
-                                longestLength = StringLength(longestLength, result[i]);
-                            }
+                            longestLength = result[i];
                         }
-                        Console.WriteLine(longestLength);
-
-
+                        if (i > 0)
+                        {
+                            longestLength = StringLength(longestLength, result[i]);
+                        }
                     }
-                    Console.ReadLine();
-                }
-            }
+                    Console.WriteLine(longestLength);
 
-            static string StringLength(string x, string y)
-            {
-                int xLength = x.Length;
-                int yLength = y.Length;
-                if (xLength == yLength)
-                {
-                    return x;
+
                 }
-                else
-                {
-                    return x.Length > y.Length ? x : y;
-                }
-                
+                Console.ReadLine();
             }
         }
+
+        static string StringLength(string x, string y)
+        {
+            int xLength = x.Length;
+            int yLength = y.Length;
+            if (xLength == yLength)
+            {
+                return x;
+            }
+            else
+            {
+                return x.Length > y.Length ? x : y;
+            }
+                
+        }
+        
     }
 }
