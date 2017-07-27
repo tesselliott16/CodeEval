fb175a57be6d7517acc9b3dad7eec4c619066795
Tess L. Elliott <tlelliott16@gmail.com>
2017-07-27 16:43:54 -0400

    added the HiddenDigits project, with the file generator that creates a text file with random strings, then passes it into the HiddenDigits project to detect the code numbers within the random strings

---
+++ CodeEval/HiddenDigits/HiddenDigits.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HiddenDigits
+{
+    class HiddenDigits
+    {
+        static void Main(string[] args)
+        {
+            GenerateRandomString.CreateFile(50);
+            string f = GenerateRandomString.FileName.GeneratedFilePath;
+            var list = new List<string>();
+            var fileStream = new FileStream(f, FileMode.Open, FileAccess.Read);
+            using (var reader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    List<char> hiddenList = new List<char>() {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+                    List<int> hiddenDigits = new List<int>();
+                    char[] designate = line.ToCharArray();
+                    foreach (char letter in designate)
+                    {
+                        if (char.IsLower(letter) && hiddenList.Contains(letter))
+                        {
+                            hiddenDigits.Add(hiddenList.IndexOf(letter));
+                        }
+                        if (char.IsDigit(letter))
+                        {;
+                            hiddenDigits.Add(Convert.ToInt32(char.GetNumericValue(letter)));
+                        }
+                    }
+                    if (hiddenDigits.Count == 0)
+                    {
+                        Console.WriteLine("NULL");
+                    }
+                    else
+                    {
+                        StringBuilder builder = new StringBuilder();
+                        foreach (int digit in hiddenDigits)
+                        {
+                            builder.Append(digit).Append("");
+                        }
+                        Console.WriteLine(builder.ToString());
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodeEval/HiddenDigits/StringGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace HiddenDigits
+{
+    class GenerateRandomString
+    {
+        public static Random random = new Random();
+        public static void CreateFile(int numberOfStrings)
+        {
+            string line = string.Empty;
+            int stringLength = random.Next(5, 15);
+            FileName.GeneratedFilePath = @"C:\_Projects\CodeEval\CodeEval\HiddenDigits\GeneratedText\GeneratedText" + stringLength + ".txt";
+            if (!File.Exists(FileName.GeneratedFilePath))
+            {
+                File.Create(FileName.GeneratedFilePath).Dispose();
+                using (var file = new System.IO.StreamWriter(FileName.GeneratedFilePath))
+                {
+                    for (int i = 0; i < numberOfStrings; i++)
+                    {
+                        line = RandomString(stringLength);
+                        file.WriteLine(line);
+                    }
+                }
+            }
+            if (File.Exists(FileName.GeneratedFilePath))
+            {
+                File.WriteAllText(FileName.GeneratedFilePath, string.Empty);
+                using (var file = new System.IO.StreamWriter(FileName.GeneratedFilePath))
+                {
+                    for (int i = 0; i < numberOfStrings; i++)
+                    {
+                        line = RandomString(stringLength);
+                        file.WriteLine(line);
+                    }
+                }
+            }
+        }
+        public static string RandomString(int length)
+        {
+            const string chars = @"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789[]{};':,./<>?!@#define$%^&*()\|=+`~";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static class FileName
+        {
+            public static string GeneratedFilePath { get; set; }
+        }
+    }
+}
+
