5c4cf3e621328aa5646458b3f489f7290df4e3e7
Tess L. Elliott <tlelliott16@gmail.com>
2017-07-28 09:08:20 -0400

    added completed project SwapCase which swaps the case of each character in a sentence

---
+++ CodeEval/SwapCase/SwapCaseProgram.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapCase
+{
+    class SwapInputCase
+    {
+        static void Main(string[] args)
+        {
+            string f = "SwapCaseFile.txt";
+            List<char> finalList = new List<char>();
+            var fileStream = new FileStream(f, FileMode.Open, FileAccess.Read);
+            using (var reader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    
+                    char[] inputLine = line.ToCharArray();
+
+                    foreach (char letter in inputLine)
+                    {
+                        var letterAdjusted = new char();
+                        if (char.IsLower(letter))
+                        {
+                            letterAdjusted = char.ToUpper(letter);
+                            finalList.Add(letterAdjusted);
+                            continue;
+                        }
+                        if (char.IsUpper(letter))
+                        {
+                            letterAdjusted = char.ToLower(letter);
+                            finalList.Add(letterAdjusted);
+                            continue;
+                        }
+                        else
+                        {
+                            finalList.Add(letter);
+                            continue;
+                        }
+                    }
+                    StringBuilder builder = new StringBuilder();
+                    foreach (char letter in finalList)
+                    {
+                        builder.Append(letter);
+                    }
+                    Console.WriteLine(builder.ToString());
+                    builder.Clear();
+                    finalList.Clear();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
