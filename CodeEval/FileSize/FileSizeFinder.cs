3657648569aaf4f69eb44ce6f330fa4d63597b94
Tess L. Elliott <tlelliott16@gmail.com>
2017-08-04 15:41:39 -0400

    created a FileSizeFinder which generates a file with a given number of strings that are a up to a certain length

---
+++ CodeEval/FileSize/FileSizeFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringGenerator;
+
+namespace FileSize
+{
+    class FileSizeFinder
+    {
+        static void Main(string[] args)
+        {
+            GenerateRandomString.CreateFile(1000, GenerateRandomString.GenerateType.Numeric, 5);
+            string f = GenerateRandomString.FileName.GeneratedFilePath;
+            var fileStream = new FileStream(f, FileMode.Open, FileAccess.Read);
+            using (var reader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                FileInfo info = new FileInfo(f);
+                long fileLength = f.Length;
+                Console.WriteLine(fileLength);
+            }
+            Console.ReadLine();
+        }
+    }
+}
