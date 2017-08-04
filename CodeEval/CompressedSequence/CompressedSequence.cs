fd586e06a8ee1fbdba2195d72e09b88d2b24cf8e
Tess L. Elliott <tlelliott16@gmail.com>
2017-08-04 15:30:22 -0400

    edits and updates to the MajorElement project and the completion of the CompressedSequences project"

--- CodeEval/CompressedSequence/CompressedSequence.cs
+++ CodeEval/CompressedSequence/CompressedSequence.cs
@@ -5,14 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MajorElement
+namespace CompressedSequence
 {
-    class MajorElement
+    class CompressedSequence
     {
         static void Main(string[] args)
         {
-            string f = "MajorElementsFile.txt";
+            string f = "CompressedSequenceFile.txt";
             var list = new List<string>();
+            StringBuilder builder = new StringBuilder();
             var fileStream = new FileStream(f, FileMode.Open, FileAccess.Read);
             using (var reader = new StreamReader(fileStream, Encoding.UTF8))
             {
@@ -20,25 +21,22 @@ namespace MajorElement
                 while ((line = reader.ReadLine()) != null)
                 {
                     int count;
+                    List<int> numbersAll = new List<int>();
                     List<int> numbers = new List<int>();
                     string isMajor = String.Empty;
-                    string[] stringNumbers = line.Split(',');
+                    string[] stringNumbers = line.Split(' ');
                     foreach (var number in stringNumbers)
                     {
-                        numbers.Add(Convert.ToInt32(number));
+                        numbersAll.Add(Convert.ToInt32(number));
                     }
+                    numbers = numbersAll.Distinct().ToList();
                     for (int i = 0; i < numbers.Count; i++)
                     {
                         int number = numbers[i];
-                        count = numbers.Count(x => x == number);
-                        if (count > numbers.Count / 2)
-                        {
-                            isMajor = number.ToString();
-                            break;
-                        }
-                        isMajor = "None\n";
+                        count = numbersAll.Count(x => x == number);
+                        builder.Append(count + " " + number).Append(" ");
                     }
-                    Console.WriteLine(isMajor);
+                    Console.WriteLine(builder.ToString());
                 }
             }
             Console.ReadLine();
