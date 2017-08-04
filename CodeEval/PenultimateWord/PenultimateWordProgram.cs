08806edee6466095a02ab1b61cf5fbabf96da8e9
Tess L. Elliott <tlelliott16@gmail.com>
2017-08-04 15:34:51 -0400

    added the completed PenultimateWord project which prints the penultimate word of every line in a provided text file

---
+++ CodeEval/PenultimateWord/PenultimateWordProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenultimateWord
+{
+    class PenultimateWordProgram
+    {
+        static void Main(string[] args)
+        {
+            string f = "PenultimateWordFile.txt";
+            var list = new List<string>();
+            var fileStream = new FileStream(f, FileMode.Open, FileAccess.Read);
+            using (var reader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] words = line.Split(' ');
+                    Console.WriteLine(words[words.Length - 2]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
